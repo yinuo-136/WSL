22c432a5ff019711043beb872268013724a4d429
Blue <blue@bluecode.fr>
2022-04-29 14:55:54 -0700

    Add logic to copy .wslconfig

--- diagnostics/collect-wsl-logs.ps1
+++ diagnostics/collect-wsl-logs.ps1
@@ -19,14 +19,23 @@ if ($LogProfile -eq $null)
 
 reg.exe export HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Lxss $folder/HKCU.reg
 reg.exe export HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Lxss $folder/HKLM.reg
+reg.exe export HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\P9NP $folder/P9NP.reg
+
+$wslconfig = "$env:USERPROFILE/.wslconfig"
+if (Test-Path $wslconfig)
+{
+    Copy-Item $wslconfig $folder
+}
 
 get-appxpackage MicrosoftCorporationII.WindowsSubsystemforLinux > $folder/appxpackage.txt
-get-acl "C:\ProgramData\Microsoft\Windows\WindowsApps" > $folder/acl.txt
+get-acl "C:\ProgramData\Microsoft\Windows\WindowsApps" | Format-List > $folder/acl.txt
 
 wpr.exe -start $LogProfile -filemode 
 
 Write-Host -NoNewLine -ForegroundColor Green "Log collection is running. Please reproduce the problem and press any key to save the logs."
 $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+
+Write-Host "`nSaving logs..."
 
 wpr.exe -stop $folder/logs.etl
 $logArchive = "$(Resolve-Path $folder).zip"
